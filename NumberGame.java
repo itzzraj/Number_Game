66e7599fb2a312bd831ff68f3fe221a138f4018d
itzzraj <108891125+itzzraj@users.noreply.github.com>
2023-09-09 15:32:03 +0530

    Add files via upload

---
+++ NumberGame.java
@@ -0,0 +1,54 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class NumberGame {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        int lowerBound = 1;
+        int upperBound = 100;
+        int maxAttempts = 10;
+        int rounds = 0;
+        int score = 0;
+
+        boolean playAgain = true;
+
+        while (playAgain) {
+            int targetNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+            System.out.println("Round " + (rounds + 1));
+            System.out.println("Guess the number between " + lowerBound + " and " + upperBound);
+
+            int attempts = 0;
+            boolean guessedCorrectly = false;
+
+            while (attempts < maxAttempts) {
+                System.out.print("Enter your guess: ");
+                int userGuess = scanner.nextInt();
+                attempts++;
+
+                if (userGuess == targetNumber) {
+                    guessedCorrectly = true;
+                    break;
+                } else if (userGuess < targetNumber) {
+                    System.out.println("Too low!");
+                } else {
+                    System.out.println("Too high!");
+                }
+            }
+
+            if (guessedCorrectly) {
+                System.out.println("Congratulations! You guessed the number in " + attempts + " attempts.");
+                score++;
+            } else {
+                System.out.println("Sorry, you didn't guess the number. The correct number was " + targetNumber);
+            }
+
+            rounds++;
+            System.out.print("Do you want to play again? (yes/no): ");
+            String playAgainInput = scanner.next().toLowerCase();
+            playAgain = playAgainInput.equals("yes");
+        }
+
+        System.out.println("Game over! Your final score is: " + score);
+    }
+}
